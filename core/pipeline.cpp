9e23b647bcb56420c718e39a6e6aa38fc26e380b
ianmcgregor1 <imcgregor@umass.edu>
2026-04-08 17:26:05 -0400

    Fixed load/store float bug

--- core/pipeline.cpp
+++ core/pipeline.cpp
@@ -348,8 +348,16 @@ Instruction Pipeline::decode(bool prevStalled) {
     } else {
       // No hazard - read values from register file
       if (decInst.isFloat) {
-        decInst.fs1_value = regs->readFloat(decInst.rs1);
-        decInst.fs2_value = regs->readFloat(decInst.rs2);
+        if (decInst.type == InstructionType::LOAD_STORE) {
+            // Memory address is always an integer
+            decInst.rs1_value = regs->readInt(decInst.rs1);
+            if (decInst.funct3 == static_cast<uint8_t>(LoadStoreFunct3::STORE)) {
+                decInst.fs2_value = regs->readFloat(decInst.rs2); // Read data to store
+            }
+        } else {
+            decInst.fs1_value = regs->readFloat(decInst.rs1);
+            decInst.fs2_value = regs->readFloat(decInst.rs2);
+        }
       }
       else {
         decInst.rs1_value = regs->readInt(decInst.rs1);
